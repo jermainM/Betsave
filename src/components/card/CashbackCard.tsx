a7af64abcef3dc870082d229cee59b29adac8e9b
voldemote <102214141+hederan@users.noreply.github.com>
2025-02-14 11:57:53 +0900

    cashback card image size changed

--- src/components/card/CashbackCard.tsx
+++ src/components/card/CashbackCard.tsx
@@ -8,8 +8,8 @@ interface CashbackCardProps {
 export const CashbackCard = (props: CashbackCardProps) => {
   const { img } = props;
   return (
-    <CashbackCardContainer>
-      <Img src={img} alt="card-img" />
+    <CashbackCardContainer img={img}>
+      {/* <Img src={img} alt="card-img" /> */}
       {/* <CashbackCardTitle>Bet</CashbackCardTitle>
       <CashbackCardText>$1,000/month</CashbackCardText>
       <CashbackCardSubTitle>
@@ -19,26 +19,43 @@ export const CashbackCard = (props: CashbackCardProps) => {
   );
 };
 
-const CashbackCardContainer = styled(Box)(({ theme }) => ({
-  borderRadius: '15px',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  width: '100%',
-  height: '375px',
-  // backgroundColor: '#141c30',
-  background:
-    'radial-gradient(circle at 50% 100%, rgba(14, 247, 169, 0.3) 0%, #0F3D3E 10% 10%, #141c30)',
-  gap: '6px',
-  position: 'relative',
-  overflow: 'hidden',
-  [theme.breakpoints.down(960)]: {
-    height: '280px',
-  },
-  [theme.breakpoints.down(390)]: {
-    height: '220px',
-  },
-}));
+const CashbackCardContainer = styled(Box)<{ img: string }>(
+  ({ theme, img }) => ({
+    borderRadius: '15px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+    height: '375px',
+    // backgroundColor: '#141c30',
+    // background:
+    //   'radial-gradient(circle at 50% 100%, rgba(14, 247, 169, 0.3) 0%, #0F3D3E 10% 10%, #141c30)',
+    backgroundImage: `url(${img})`,
+    backgroundPosition: 'center center',
+    backgroundSize: 'cover',
+    gap: '6px',
+    position: 'relative',
+    overflow: 'hidden',
+    [theme.breakpoints.down(1024)]: {
+      height: '320px',
+    },
+    [theme.breakpoints.down(870)]: {
+      height: '280px',
+    },
+    [theme.breakpoints.down(768)]: {
+      height: '420px',
+    },
+    [theme.breakpoints.down(640)]: {
+      height: '360px',
+    },
+    [theme.breakpoints.down(480)]: {
+      height: '280px',
+    },
+    [theme.breakpoints.down(390)]: {
+      height: '220px',
+    },
+  })
+);
 
 const CashbackCardTitle = styled(Typography)(({ theme }) => ({
   color: '#627691',
